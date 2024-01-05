85ed558e8abf20e78588d402d58c14626e8c826d
kratikesh18 <im.kartikesh18@gmail.com>
2024-01-06 01:53:36 +0530

    fixing issue test1

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,7 +2,7 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-import Container from "./Container/Container";
+import Container from "./container/Container";
 import RTE from "./RTE";
 import SignUp from './SignUp'
 import Select from './Select'
