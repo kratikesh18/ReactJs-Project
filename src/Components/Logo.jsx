b9e284088348ba9855fe28c532707685196b6278
kratikesh18 <im.kartikesh18@gmail.com>
2024-01-11 16:08:17 +0530

    modified logo

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo({width = "100px"}) {
   return (
-    <div className=" flex justify-center items-center text-yellow-500
+    <div className=" flex justify-center items-center text-orange-400
      bg-black/90 
      w-[100px] p-[0.4rem]  font-bold rounded-md">Blogsssy</div>
   )
