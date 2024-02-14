a0aef27ed698e7555e9aaf946ae6e9d285c67563
kratikesh18 <im.kartikesh18@gmail.com>
2024-02-14 15:39:47 +0530

    fixed confilcts"

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import store from "./Store/Store.js";
-import { Routes, Route, BrowserRouter, RouterProvider } from "react-router-dom";
-import UnderConstruction from "./Pages/UnderConstruction.jsx";
-import LoginPage from "./Pages/LoginPage.jsx";
+import store from "./Store/store.js";
+import { BrowserRouter} from "react-router-dom";
+
 
 
 
