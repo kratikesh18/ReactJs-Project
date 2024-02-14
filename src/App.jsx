28ffa099e5afbe43cd3208580eb799b693e4131d
kratikesh18 <im.kartikesh18@gmail.com>
2024-02-14 15:47:00 +0530

    fixed confilcts"

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Layout from "./Pages/Layout.jsx";
 import LoginPage from "./Pages/LoginPage.jsx";
 import UnderConstruction from "./Pages/UnderConstruction.jsx";
 import SignUpPage from "./Pages/SignUpPage.jsx";
 import AllPost from "./Pages/AllPost.jsx";
 import Post from "./Pages/Post.jsx";
+import Layout from "./Pages/Layout.jsx";
 
 function App() {
   return (
     <div>
-      <Layout />
+      <Layout />;
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
@@ -18,7 +18,6 @@ function App() {
         <Route path="/post/:slug" element={<Post />} />
         <Route path="*" element={<UnderConstruction />} />
       </Routes>
-      ;
     </div>
   );
 }
